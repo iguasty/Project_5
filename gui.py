3634e818fd3a97ddfa806cc868c8ea0c7cf5bda2
iguasty <127635798+iguasty@users.noreply.github.com>
2024-05-27 14:40:39 -0700

    Added grid to gui.py

--- gui.py
+++ gui.py
@@ -6,18 +6,19 @@ class Gui:
         self.root = tk.Tk(className="num_search")
         self.root.minsize(800, 600)
 
-        self.title_label = tk.Label(self.root, text="Search for a value!")
-        self.prompt_label = tk.Label(self.root, text="Type in a value to search for it in this list array")
-        self.entry_box = tk.Entry(self.root)
-        self.enter_button = tk.Button(self.root, text="Enter", command=self.update_label)
+        # self.title_label = tk.Label(self.root, text="Search for a value!")
+        # self.prompt_label = tk.Label(self.root, text="Type in a value to search for it in this list array")
+        # self.entry_box = tk.Entry(self.root)
+        # self.enter_button = tk.Button(self.root, text="Enter", command=self.update_label)
         
         self.create_num_array()
         print(self.create_num_array())
         
-        self.title_label.pack()
-        self.prompt_label.pack()
-        self.entry_box.pack()
-        self.enter_button.pack()
+        # self.title_label.pack()
+        # self.prompt_label.pack()
+        # self.box.pack()
+        # self.enter_button.pack()
+        self.create_boxes()
         self.root.mainloop()
         
     def update_label(self):
@@ -33,6 +34,37 @@ class Gui:
             num_array.append(val)
         return num_array
     
+    def create_boxes(self):
+        self.entry_box = tk.Entry(self.root)
+        # self.box = tk.Label(self.root, text="Test") 
+        self.boxes = [] #list of entry boxes
+        rows = 10
+        columns = 10
+        #creates 10 text boxes and appends them to list
+        
+        for i in range(rows):
+            row_boxes = []
+            # self.box = tk.Label(self.root, text="Tester")
+            # self.box.pack(side="top")
+            # self.boxes.append(self.entry_box)        
+            for j in range(columns):
+                self.box = tk.Label(self.root, text="Test")
+                self.box.grid(row=i, column=j, padx=5, pady=5)
+                row_boxes.append(self.entry_box) 
+            self.boxes.append(row_boxes)
+        
+        self.prompt_label = tk.Label(self.root, text="Type in a value to search for it in this list array")    
+        self.enter_button = tk.Button(self.root, text="Enter", command=self.update_label)
+        self.entry_box.grid(row=rows, column=0, columnspan=columns-1, sticky="nsew")
+        self.prompt_label.grid(row=rows+1, column=0, columnspan=columns-1, sticky="nsew")
+        self.enter_button.grid(row=rows+2, column=0, columnspan=columns-1, sticky="nsew")
+        
+        # self.title_label = tk.Label(self.root, text="Search for a value!")
+                
+        # self.title_label.pack(side="bottom")
+        
+    
+        
     def run(self):
         self.root.mainloop()
         
